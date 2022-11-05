acaae7cf6ac710e05843b201132784918d412574
Yevhenii Pozniak <jekamanu@gmail.com>
2022-11-05 18:56:39 +0200

    update Step 2

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -27,6 +27,14 @@ export class Feedback extends Component {
     this.setState(prevState => ({ bad: prevState.bad + 1 }));
   };
 
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
   render() {
     const { good } = this.state;
     const { neutral } = this.state;
@@ -58,6 +66,18 @@ export class Feedback extends Component {
           </li>
           <li>
             <Text>Bad: {bad}</Text>
+          </li>
+          <li>
+            <Text>Total: {this.countTotalFeedback()}</Text>
+          </li>
+          <li>
+            <Text>
+              Positive feedback:{' '}
+              {this.countPositiveFeedbackPercentage()
+                ? this.countPositiveFeedbackPercentage()
+                : 0}
+              %
+            </Text>
           </li>
         </TextList>
       </Wrapper>
